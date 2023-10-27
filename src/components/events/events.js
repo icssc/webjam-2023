74c161874955f94fc718fc6c67e7d89db68277b2
Kevin Wu <kevinwu098@gmail.com>
2023-10-26 17:09:39 -0700

    Create events section

---
+++ src/components/events/events.js
@@ -0,0 +1,11 @@
+import { FaCalendar, FaLocationArrow } from "react-icons/fa";
+
+export default function EventsSection() {
+  return (
+    <>
+      <div>
+        <div>EVENTS</div>
+      </div>
+    </>
+  );
+}
