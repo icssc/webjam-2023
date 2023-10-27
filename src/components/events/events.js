9dc69c8ea1f1b4e8a50b4f6b9c24b9b825e89e0c
Kevin Wu <kevinwu098@gmail.com>
2023-10-26 23:23:34 -0700

    Clean up responsiveness

--- src/components/events/events.js
+++ src/components/events/events.js
@@ -2,7 +2,6 @@ import { FaExternalLinkAlt } from "react-icons/fa";
 import EventsData from "../../assets/data/events.json";
 
 function getFormattedDate(date) {
-  console.log(date);
   const dateObject = new Date(date + "T00:00:00").toLocaleDateString("en-US", {
     timeZone: "America/Los_Angeles",
     weekday: "long",
@@ -13,7 +12,7 @@ function getFormattedDate(date) {
   return dateObject;
 }
 
-function getSortedEvents(schedule) {
+function getSortedSchedule(schedule) {
   const sortedSchedule = {};
 
   schedule.forEach((event) => {
@@ -31,35 +30,35 @@ function getSortedEvents(schedule) {
 
 export default function EventsSection() {
   const schedule = EventsData.schedule;
-  const sortedEvents = getSortedEvents(schedule);
-
-  console.log(sortedEvents);
+  const sortedSchedule = getSortedSchedule(schedule);
 
   return (
     <>
-      <div className="flex flex-col mx-8 md:mx-24 my-10 w-[100%] gap-4">
-        <div className="flex place-content-start text-6xl">Schedule</div>
+      <div className="flex flex-col mx-8 md:mx-24 my-10 gap-2 md:gap-4">
+        <div className="flex place-content-start text-5xl md:text-6xl">
+          Schedule
+        </div>
 
-        {sortedEvents.map((date) => (
-          <div className="flex flex-col gap-1">
+        {sortedSchedule.map((date) => (
+          <div className="flex flex-col gap-1 mb-2 md:mb-6">
             <div className="text-3xl lg:text-4xl text-gray-400">
               {getFormattedDate(date[0].date)}
             </div>
 
             {date.map((event) => (
-              <div className="flex flex-col lg:flex-row border-gray-400 border-y-4 border-dashed w-[100%] py-6 mb-6 lg:items-center lg:justify-between gap-6 lg:gap-0">
+              <div className="flex flex-col lg:flex-row border-gray-400 border-t-4 border-dashed w-[100%] py-4 lg:items-center lg:justify-between gap-6 lg:gap-0">
                 <div className="flex gap-6">
-                  <div className="flex items-center h-32 w-32">
+                  <div className="md:flex items-center h-32 w-32 hidden md:visible">
                     <img
                       src={event.logo}
                       alt="Club Logo"
-                      className="rounded-2xl flex"
+                      className="flex rounded-2xl"
                     />
                   </div>
 
                   <div className="flex flex-col gap-4">
                     <div>
-                      <div className="flex place-content-left text-3xl md:text-4xl text-white">
+                      <div className="flex text-3xl md:text-4xl text-white lg:w-[20rem] xl:w-[32rem]">
                         {event.title}
                       </div>
                       <div className="text-lg md:text-xl">
@@ -67,12 +66,12 @@ export default function EventsSection() {
                       </div>
                     </div>
 
-                    <div className="flex gap-4">
+                    <div className="flex gap-2 md:gap-4">
                       {event.rsvp && (
                         <a href="" target="_blank" className="flex">
                           <div className="bg-green-600 flex rounded-full drop-shadow-lg place-content-center text-xl px-4 h-10 items-center gap-2">
                             RSVP
-                            <span>
+                            <span className="md: visible hidden">
                               <FaExternalLinkAlt />
                             </span>
                           </div>
