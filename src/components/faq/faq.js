24732df6b0aaba806e7442d61d029f8469dc8b26
Kevin Wu <kevinwu098@gmail.com>
2023-11-12 02:07:06 -0800

    Integrate level 3

--- src/components/faq/faq.js
+++ src/components/faq/faq.js
@@ -1,41 +1,81 @@
-import faq_data from '../../../assets/faq.json'
-import styles from '../../styles/components/Faq.module.css'
+import faq_data from "../../../assets/faq.json";
+import styles from "../../styles/components/Faq.module.css";
 
 function FAQTab(props) {
-  const {title, content, id} = props;
+  const { title, content, id } = props;
   return (
-  <div className={styles.tab}>
-    <input type="checkbox" name={styles.accordion} id={id} />
-    <label htmlFor={id} className={styles.tab__label}>{title}</label>
-    <div className={styles.tab__content}>
-      <p className='text-xl'>{content}</p>
+    <div className={styles.tab}>
+      <input type="checkbox" name={styles.accordion} id={id} />
+      <label htmlFor={id} className={styles.tab__label}>
+        {title}
+      </label>
+      <div className={styles.tab__content}>
+        <p className="text-xl">{content}</p>
+      </div>
     </div>
-  </div>
-  )
+  );
 }
 
 export default function FAQSection() {
   return (
-    <div className="flex flex-col items-center gap-4 mt-5">
-    <div className="text-4xl md:text-7xl">FAQ</div>
-    <div className={`${styles.accordion} md:max-w-[50%] `}>
-      {faq_data.map((element, index) => <FAQTab key={index} id={index} {...element}/>)}
-      {/* Hardcoding some FAQ tabs to include clickable URLs */}
-      <div className={styles.tab}>
-        <input type="checkbox" name={styles.accordion} id={faq_data.length} />
-        <label htmlFor={faq_data.length} className={styles.tab__label}>What are the rules?</label>
-        <div className={styles.tab__content}>
-          <p className='text-xl'>Please refer to the <a target="_blank" href="https://docs.google.com/document/d/1T2Dh_FVx0zU8xhNC2CHDMb_0xg-j2oFBUfp-M0C4FqM/edit">WebJam Rules and Code of Conduct</a> for more information.</p>
+    <>
+      <div className="z-50 mt-16 flex flex-col items-center gap-4 md:mt-32">
+        <div className="text-4xl md:text-7xl">FAQ</div>
+        <div className={`${styles.accordion} md:max-w-[50%] `}>
+          {faq_data.map((element, index) => (
+            <FAQTab key={index} id={index} {...element} />
+          ))}
+          {/* Hardcoding some FAQ tabs to include clickable URLs */}
+          <div className={styles.tab}>
+            <input
+              type="checkbox"
+              name={styles.accordion}
+              id={faq_data.length}
+            />
+            <label htmlFor={faq_data.length} className={styles.tab__label}>
+              What are the rules?
+            </label>
+            <div className={styles.tab__content}>
+              <p className="text-xl">
+                Please refer to the{" "}
+                <a
+                  target="_blank"
+                  href="https://docs.google.com/document/d/1T2Dh_FVx0zU8xhNC2CHDMb_0xg-j2oFBUfp-M0C4FqM/edit"
+                >
+                  WebJam Rules and Code of Conduct
+                </a>{" "}
+                for more information.
+              </p>
+            </div>
+          </div>
+          <div className={styles.tab}>
+            <input
+              type="checkbox"
+              name={styles.accordion}
+              id={faq_data.length + 1}
+            />
+            <label htmlFor={faq_data.length + 1} className={styles.tab__label}>
+              My question isn't here!
+            </label>
+            <div className={styles.tab__content}>
+              <p className="text-xl">
+                Unanswered question? Email us at{" "}
+                <a target="_blank" href="mailto:icssc@uci.edu">
+                  icssc@uci.edu
+                </a>
+                .
+              </p>
+            </div>
+          </div>
         </div>
       </div>
-      <div className={styles.tab}>
-        <input type="checkbox" name={styles.accordion} id={faq_data.length+1} />
-        <label htmlFor={faq_data.length+1} className={styles.tab__label}>My question isn't here!</label>
-        <div className={styles.tab__content}>
-          <p className='text-xl'>Unanswered question? Email us at <a target="_blank" href="mailto:icssc@uci.edu">icssc@uci.edu</a>.</p>
-        </div>
-      </div>
-    </div>
-    </div>
-  )
+
+      <div
+        className={`${styles.bottomCeiling} absolute left-0 top-0 z-30 w-[100%]`}
+      />
+      <div
+        className={`${styles.anglerfish} absolute bottom-[40%] right-[10%] w-[25vw] sm:bottom-[12.5rem] sm:right-24 md:bottom-0 md:right-[.5rem] lg:w-[25vw]`}
+      />
+    </>
+  );
 }
--- src/components/footer.js
+++ src/components/footer.js
@@ -7,6 +7,7 @@ import {
 } from "react-icons/fa";
 import Link from "next/link";
 import Clubs from "../../assets/clubs.json";
+import styles from "../styles/Footer.module.css";
 
 const MEDIA_LINKS = [
   {
@@ -42,11 +43,7 @@ const ClubElement = (props) => {
     <div className="flex items-center text-center">
       <div className="w-[100px]">
         <Link href={website ? website : ""} target="_blank" rel="noreferrer">
-          <img
-            src={logo}
-            alt={name}
-            className="rounded-lg"
-          />
+          <img src={logo} alt={name} className="rounded-lg" />
         </Link>
       </div>
     </div>
@@ -55,37 +52,46 @@ const ClubElement = (props) => {
 
 export default function Footer() {
   return (
-    <div id="footer" className="flex flex-col gap-4 sm:gap-10 pt-[10vh]">
-      {/* <div className="flex flex-col gap-4">
-        <span className="flex justify-center text-4xl md:text-7xl">Thank You to Our Partners!</span>
-        <div className="flex flex-row mx-auto overflow-x-scroll w-[50%] gap-6">
-          {Clubs.map((club) => (
-            <ClubElement {...club} key={club.id} />
-          ))}
-        </div>
-      </div> */}
-      <footer className="w-[100%] flex flex-col gap-5 p-6">
-        <div className="flex justify-center gap-5 sm:gap-12 text-3xl sm:text-4xl">
-          {MEDIA_LINKS.map((link) => (
-            <Link
-              href={link.link}
-              target="_blank"
-              referrerPolicy="noreferrer"
-              key={link.id}
-            >
-              {link.icon}
-            </Link>
-          ))}
-        </div>
-        <div className="flex flex-col">
-          <div className="flex justify-center text-lg sm:text-2xl">
-            Made with 💖 by the ICSSC team
+    <>
+      <div id="footer" className="flex flex-col gap-4 py-36 sm:gap-10">
+        {/* <div className="flex flex-col gap-4">
+          <span className="flex justify-center text-4xl md:text-7xl">Thank You to Our Partners!</span>
+          <div className="flex flex-row mx-auto overflow-x-scroll w-[50%] gap-6">
+            {Clubs.map((club) => (
+              <ClubElement {...club} key={club.id} />
+            ))}
           </div>
-          <div className="flex justify-center text-sm sm:text-md">
-            Copyright © 2023 ICSSC
+        </div> */}
+        <footer className="flex w-[100%] flex-col gap-5 p-6">
+          <div className="flex justify-center gap-5 text-3xl sm:gap-12 sm:text-4xl">
+            {MEDIA_LINKS.map((link) => (
+              <Link
+                href={link.link}
+                target="_blank"
+                referrerPolicy="noreferrer"
+                key={link.id}
+              >
+                {link.icon}
+              </Link>
+            ))}
           </div>
-        </div>
-      </footer>
-    </div>
+          <div className="flex flex-col">
+            <div className="flex justify-center text-lg sm:text-2xl">
+              Made with 💖 by the ICSSC team
+            </div>
+            <div className="sm:text-md flex justify-center text-sm">
+              Copyright © 2023 ICSSC
+            </div>
+          </div>
+        </footer>
+
+        <div
+          className={`${styles.bottomFloor} absolute bottom-0 left-0 z-30 h-[100%] w-[100%]`}
+        />
+        <div
+          className={`${styles.treasure} absolute bottom-0 left-[5%] z-30 h-[100%] w-[25vw] lg:left-[10%]`}
+        />
+      </div>
+    </>
   );
 }
