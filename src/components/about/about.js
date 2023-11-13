3d7dbfd89c9a6363eae4be44cfb22b7ac2a947a0
Kevin Wu <kevinwu098@gmail.com>
2023-11-13 00:28:16 -0800

    feat: Integrate layer two and three (#16)
    * Integrate new anteater, adjust styling
    
    * Add middle ceiling
    
    * Implement layer two fully
    
    * Fix width
    
    * Integrate level 3
    
    * Adjust background for contrast
    
    * svg scaling
    
    * white text on deep ocean layer
    
    * Increase sand layer, adjust dolphin and middleCeiling
    
    * Revert "Increase sand layer, adjust dolphin and middleCeiling"
    
    This reverts commit 13552ee7da86792c748ede57fd60a175873168d7.
    
    * Move layer2 down to events
    
    * Adjust eel
    
    * Adjust dolphin
    
    * remove dummy text
    
    ---------
    
    Co-authored-by: sirAvent <chiu7a@gmail.com>

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -1,18 +1,35 @@
+import styles from "../../styles/components/About.module.css";
+
 export default function AboutSection() {
   return (
-    <div className="flex flex-col items-center mx-[10%] md:mx-[20%] gap-4 my-20">
-      <div className="text-4xl md:text-7xl">About WebJam</div>
-      <div className="text-xl sm:text-2xl md:text-2xl md:leading-relaxed text-start font-light">
-        <p>
-          WebJam is a week-long event where students come together to create fully functional web applications.
-          Whether you've never heard of web applications or built many, WebJam is here to give you a platform to learn, build, and create!
-        </p>
-        <br/>
-        <p>
-          Over the past 3 years, WebJam has focused on encouraging beginners and people who are curious about web technologies — from frontend to backend!
-          Our goal is to provide learning and networking opportunities for our community of developers, especially ones that come from underrepresented backgrounds in the tech space.
-        </p>
+    <>
+      <div className="z-50 mx-[10%] flex flex-col items-center gap-4 pb-24 md:mx-[20%] md:pb-36 md:pt-24">
+        <div className="text-center text-4xl md:text-7xl">About WebJam</div>
+        <div className="text-center text-xl font-light sm:text-2xl md:text-2xl md:leading-relaxed lg:text-start">
+          <p>
+            WebJam is a week-long event where students come together to create
+            fully functional web applications. Whether you've never heard of web
+            applications or built many, WebJam is here to give you a platform to
+            learn, build, and create!
+          </p>
+          <br />
+          <p>
+            Over the past 3 years, WebJam has focused on encouraging beginners
+            and people who are curious about web technologies — from frontend to
+            backend! Our goal is to provide learning and networking
+            opportunities for our community of developers, especially ones that
+            come from underrepresented backgrounds in the tech space.
+          </p>
+        </div>
       </div>
-    </div>
+
+      <div
+        className={`${styles.middleTop} absolute left-0 top-0 z-30 w-[100%]`}
+      />
+      <div
+        className={`${styles.dolphin} absolute bottom-[-45vw] left-[-15vw] w-[80vw] lg:bottom-[-22vw] lg:left-[-7.5vw] lg:w-[30vw]`}
+        // style={{ top: -scrollValue * 0.45 - 50 + "px" }}
+      />
+    </>
   );
 }
